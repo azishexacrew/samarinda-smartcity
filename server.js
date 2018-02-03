cc36923273886214a21b68b44430373e4171e0f1
azishexacrew <root@nodetech.com>
2018-02-03 03:23:21 +0000

    update

---
+++ server.js
@@ -0,0 +1,14 @@
+// server.js
+var express = require('express');  
+var app = express();  
+var server = require('http').createServer(app);  
+
+app.use(express.static(__dirname + '/public')); 
+//redirect / to our index.html file
+app.get('/', function(req, res,next) {  
+    res.sendFile(__dirname + '/public/*');
+});
+
+server.listen(4001, function(){
+  console.log('listening on *:4001');
+}); 
